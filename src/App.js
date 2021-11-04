9c5af8eeaed69fbae4d6aa296d044c646fd5cadb
Mike Agee <mike.agee@westcarygroup.com>
2021-11-04 15:13:54 -0400

    added some dir structure, and a new component and styling started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { EstimationRow } from './components/estimation-row/estimation-row.component';
 import './App.css';
 import estimations from './data/estimation.json';
 
@@ -8,18 +9,27 @@ class App extends Component {
 
   constructor() {
     super();
-    this.state = estimations
+    this.state = {
+      estimations: []
+    }
   }
-  
+
+  componentDidMount() {
+    fetch('https://my-json-server.typicode.com/michaelagee/est-project-test-json/estimations')
+      .then(response => response.json())
+      .then(estimations => this.setState({ estimations: estimations }));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <code>{ this.state.estimations.map(estimation => <h1 key={estimation.id}> { estimation.name } </h1>) }</code>
-          </p>
-          <button onClick={() => this.setState({ string: "hello rager" })}>Change Text</button>
+          <EstimationRow name="mike">
+          {this.state.estimations.map(estimation => (
+            <h1 key={estimation.id}> {estimation.name} </h1>
+          ))}
+          </EstimationRow>
         </header>
       </div>
     )
