b99df8a8e2aebd6fc45c4fff628b8749cdc35185
Mike Agee <mike.agee@westcarygroup.com>
2021-11-04 13:38:14 -0400

    added db.json file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import estimations from './data/estimation.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = estimations
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <code>{ this.state.estimations.map(estimation => <h1 key={estimation.id}> { estimation.name } </h1>) }</code>
+          </p>
+          <button onClick={() => this.setState({ string: "hello rager" })}>Change Text</button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
