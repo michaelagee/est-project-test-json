1672efa307a879d44719217708aeed4b1b51a1af
Mike Agee <mike.agee@westcarygroup.com>
2021-11-08 14:52:07 -0500

    added off canvas element to display details of an estimation

--- src/components/estimation-block/estimation-block.component.jsx
+++ src/components/estimation-block/estimation-block.component.jsx
@@ -3,22 +3,40 @@ import { EstimationListItem } from '../estimation-list-item/estimation-list-item
 import './estimation-block.styles.css';
 import { ListGroup } from 'react-bootstrap';
 import { useNavigate } from 'react-router';
+import { Button, Offcanvas } from 'react-bootstrap';
+import { useState } from 'react';
 
 const EstimationBlock = (props) => {
     let navigate = useNavigate();
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
-        <ListGroup className="dashboard-list-group">
-            {props.estimations.map(estimation => (
-                <ListGroup.Item
-                    key={estimation.id}
-                    action onClick={props.listItemClick} 
-                    onClick={() => navigate(`/estimations/${estimation.id}`)}
-                    key={estimation.id}>
-                    {estimation.name}
-                </ListGroup.Item>
-            ))}
-        </ListGroup>
+        <>
+            <ListGroup className="dashboard-list-group">
+                {props.estimations.map(estimation => (
+                    <ListGroup.Item
+                        key={estimation.id}
+                        action onClick={props.listItemClick}
+                        onClick={handleShow}
+                        // onClick={() => navigate(`/estimations/${estimation.id}`)}
+                        key={estimation.id}>
+                        {estimation.name}
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
+
+            <Offcanvas placement='end' show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    Some text as placeholder. In real life you can have the elements you
+                    have chosen. Like, text, images, lists, etc.
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
     )
 };
 
