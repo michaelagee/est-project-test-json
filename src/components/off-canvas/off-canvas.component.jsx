c794d232b2711d87be9173c7ae9b3a0f580b2fa9
Mike Agee <mike.agee@westcarygroup.com>
2021-11-09 11:44:59 -0500

    used recoil to somehow magically get the data on the off canvaas drawer

--- src/components/off-canvas/off-canvas.component.jsx
+++ src/components/off-canvas/off-canvas.component.jsx
@@ -1,33 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Component } from "react";
 import { useState } from 'react';
 import { Offcanvas, show } from "react-bootstrap";
 import { getEstimation } from '../../data/data';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { currentEstimationState } from '../../state/atoms/estimationsState'
+import EstimationDetails from "../estimation-details/estimation.details.component";
+
 
 const OffCanvasPanel = (props) => {
     let currentEstimation = useRecoilValue(currentEstimationState);
+    const setCurrentEstimation = useSetRecoilState(currentEstimationState);
     console.log('current estimation: ', currentEstimation)
     console.log(props);
-    useEffect(() => {
-        currentEstimation = props.currentEstimation
-        console.log('wtf man 2', currentEstimation);
-    });
 
     function updateEstimationData() {
+        setCurrentEstimation(props.currentEstimation);
         console.log('wtf man 1', currentEstimation)
         currentEstimation = props.currentEstimation
     }
 
     return (
-        <Offcanvas placement='end' show={props.show} onEntering={updateEstimationData} onHide={props.onHide}>
-            <Offcanvas.Header closeButton>
-                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-                <p>{currentEstimation.name}</p>
-            </Offcanvas.Body>
-        </Offcanvas>
+        <>
+            <Offcanvas placement='end' show={props.show} onEntering={updateEstimationData} onHide={props.onHide}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>{currentEstimation.name}</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <p>{currentEstimation.name}</p>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
     )
 }
 
