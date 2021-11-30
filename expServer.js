702d8acdf1a23db917a2801408c05dbf039e8f3d
michaelagee <90787753+michaelagee@users.noreply.github.com>
2021-11-29 22:43:31 -0500

    removed comments from server file

--- expServer.js
+++ expServer.js
@@ -1,16 +1,15 @@
-const express = require('express'); //Line 1
+const express = require('express');
 const db = require('./client/public/db.json');
-const app = express(); //Line 2
+const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 1020; //Line 3
+const port = process.env.PORT || 1020;
 
 app.use(cors())
 
 // This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
-app.get('/estimations', (req, res) => { //Line 9
-  res.send(db); //Line 10
-  // console.log(res)
-}); //Line 11
+app.get('/estimations', (req, res) => {
+  res.send(db);
+});
