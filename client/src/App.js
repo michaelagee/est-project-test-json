9eac14b352994e5f4e06c1c2ab412aafe8e346d7
michaelagee <90787753+michaelagee@users.noreply.github.com>
2022-01-13 23:25:46 -0500

    reverting POST /estimations route

--- client/src/App.js
+++ client/src/App.js
@@ -105,7 +105,7 @@ class App extends Component {
       estimations: newEstimationsCollection,
     });
 
-    const url = "https://ej1wmnqenl.execute-api.us-east-1.amazonaws.com/dev/estimations";
+    const url = "https://ej1wmnqenl.execute-api.us-east-1.amazonaws.com/dev/postEstimations-dev";
     axios.post(url, newEstimationsCollection).then((response) => {
       console.log('response from POST', JSON.parse(response.config.data));
     });
